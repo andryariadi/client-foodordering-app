0f4cc3c1a7da98d7b433bc88d1f6ee9a985ae987
Andry Ariadi <andryariadi23@gmail.com>
2023-12-18 16:00:00 +0700

    styling slider page

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -11,7 +11,7 @@ const data = [
   {
     id: 2,
     title: "we deliver your order wherever you are in NY",
-    image: "/slide2.png",
+    image: "/slide2.jpg",
   },
   {
     id: 3,
@@ -24,31 +24,20 @@ const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
-      4000
-    );
+    const interval = setInterval(() => setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)), 4000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
-          {data[currentSlide].title}
-        </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-rose-600 font-bold">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-5xl xl:text-6xl">{data[currentSlide].title}</h1>
+        <button className="bg-rose-600 text-white px-4 py-2 rounded">Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="w-full flex-1 relative">
-        <Image
-          src={data[currentSlide].image}
-          alt=""
-          fill
-          className="object-cover"
-        />
+        <Image src={data[currentSlide].image} alt="" fill className="object-cover" />
       </div>
     </div>
   );
