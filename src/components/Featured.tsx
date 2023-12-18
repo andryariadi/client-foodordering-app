523da369a4f0fecadebab4f643964feac5d12e2f
Andry Ariadi <andryariadi23@gmail.com>
2023-12-18 16:06:49 +0700

    styling featured page

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,34 +4,31 @@ import React from "react";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-red-500">
-      {/* WRAPPER */}
-      <div className="w-max flex">
-        {/* SINGLE ITEM */}
-        {featuredProducts.map((item) => (
-          <div
-            key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
-          >
-            {/* IMAGE CONTAINER */}
-            {item.img && (
-              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
-                <Image src={item.img} alt="" fill className="object-contain" />
+    <>
+      <div className="card-container overflow-x-scroll text-rose-600">
+        {/* WRAPPER */}
+        <div className="w-max flex">
+          {/* CARD */}
+          {featuredProducts.map((product) => (
+            <div key={product.id} className="w-screen md:w-[50vw] xl:w-[33vw] h-[60vh] xl:h-[90vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300">
+              {product.img && (
+                <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                  <Image src={product.img} alt="featured" fill className="object-contain" />
+                </div>
+              )}
+              <div className="flex-1 flex flex-col items-center justify-center text-center gap-4 py-5">
+                <h1 className="text-xl xl:text-2xl 2xl:text-3xl font-bold uppercase">{product.title}</h1>
+                <p className="p-4 2xl:p-8">{product.desc}</p>
+                <span className="text-xl font-bold">${product.price}</span>
+                <button className="bg-rose-600 text-white p-2 rounded-md">Add to Cart</button>
               </div>
-            )}
-            {/* TEXT CONTAINER */}
-            <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
-              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
-              <p className="p-4 2xl:p-8">{item.desc}</p>
-              <span className="text-xl font-bold">${item.price}</span>
-              <button className="bg-red-500 text-white p-2 rounded-md">
-                Add to Cart
-              </button>
             </div>
-          </div>
-        ))}
+          ))}
+          {/* END CARD */}
+        </div>
+        {/* END WRAPPER */}
       </div>
-    </div>
+    </>
   );
 };
 
