b5828c3e0386184d87c333948c6a91152efc24db
Andry Ariadi <andryariadi23@gmail.com>
2023-12-18 16:28:50 +0700

    styling login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,43 +4,33 @@ import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
-      {/* BOX */}
-      <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
-        {/* IMAGE CONTAINER */}
-        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
-          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
+    <>
+      <main className="flex items-center justify-center h-screen m-3">
+        <div className="flex flex-col md:flex-row h-full md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2 shadow-2xl rounded-md">
+          <div className="relative h-full md:h-full w-full md:w-1/2">
+            <Image src="/login.jpg" alt="login" fill className="object-cover" />
+          </div>
+          <div className="flex flex-col gap-8 p-10 md:w-1/2 justify-center">
+            <h1 className="text-xl xl:text-3xl font-bold">Welcome</h1>
+            <p>Log into your account or create a new one using social buttons</p>
+            <button className="flex gap-4 p-4 ring-1 ring-rose-600 rounded-md">
+              <Image src={"/google.png"} alt="google" width={20} height={20} className="object-contain" />
+              <span>Sign in with Google</span>
+            </button>
+            <button className="flex gap-4 p-4 ring-1 ring-offset-cyan-500 rounded-md">
+              <Image src={"/facebook.png"} alt="google" width={20} height={20} className="object-contain" />
+              <span>Sign in with Facebook</span>
+            </button>
+            <p className="text-sm">
+              Have a problem ?{" "}
+              <Link href={"/contact"} className="text-rose-600 font-semibold">
+                Contact us
+              </Link>
+            </p>
+          </div>
         </div>
-        {/* FORM CONTAINER */}
-        <div className="p-10 flex flex-col gap-8 md:w-1/2">
-          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
-          <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
-            <Image
-              src="/google.png"
-              alt=""
-              width={20}
-              height={20}
-              className="object-contain"
-            />
-            <span>Sign in with Google</span>
-          </button>
-          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
-            <Image
-              src="/facebook.png"
-              alt=""
-              width={20}
-              height={20}
-              className="object-contain"
-            />
-            <span>Sign in with Facebook</span>
-          </button>
-          <p className="text-sm">
-            Have a problem?<Link className="underline" href="/"> Contact us</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+      </main>
+    </>
   );
 };
 
