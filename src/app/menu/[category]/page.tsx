9d050e98d331542046e1bf210e5554e9d56df94d
Andry Ariadi <andryariadi23@gmail.com>
2023-12-18 16:14:53 +0700

    styling category page

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -5,24 +5,24 @@ import React from "react";
 
 const CategoryPage = () => {
   return (
-    <div className="flex flex-wrap text-red-500">
-      {pizzas.map((item) => (
-        <Link className="w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group odd:bg-fuchsia-50" href={`/product/${item.id}`} key={item.id}>
-          {/* IMAGE CONTAINER */}
-          {item.img && (
-            <div className="relative h-[80%]">
-              <Image src={item.img} alt="" fill className="object-contain"/>
+    <>
+      <div className="flex flex-wrap">
+        {pizzas.map((item) => (
+          <Link href={`/product/${item.id}`} key={item.id} className="w-full sm:w-1/2 lg:w-1/3 h-[60vh] border-r-2 border-b-2 border-rose-600 p-4 flex flex-col justify-between group even:bg-fuchsia-50">
+            {item.img && (
+              <div className="relative h-[80%]">
+                <Image src={item.img} alt="pizza" fill className="object-contain" />
+              </div>
+            )}
+            <div className="flex items-center justify-between font-bold text-rose-600">
+              <h1 className="text-2xl uppercase p-2">{item.title}</h1>
+              <p className="text-xl group-hover:hidden">${item.price}</p>
+              <button className="hidden bg-rose-600 text-white p-2 rounded-md group-hover:block">Add to Cart</button>
             </div>
-          )}
-          {/* TEXT CONTAINER */}
-          <div className="flex items-center justify-between font-bold">
-            <h1 className="text-2xl uppercase p-2">{item.title}</h1>
-            <h2 className="group-hover:hidden text-xl">${item.price}</h2>
-            <button className="hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md">Add to Cart</button>
-          </div>
-        </Link>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
